482397ec4227bf3a6250a7db5f40ada78d31bfc4
Martin Šimko <martin.simko.23@gmail.com>
2025-11-26 19:27:04 +0100

    Initial commit

---
+++ autobazar-backend/src/test/java/com/martin/autobazar/AutobazarBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.martin.autobazar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AutobazarBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
